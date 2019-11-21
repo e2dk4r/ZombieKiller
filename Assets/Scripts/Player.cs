edb56df41e3482ff4ff65e77925c51a3f788403e
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2019-11-21 20:17:14 +0300

    add player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    Rigidbody2D body;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var move = new Vector2(horizontal, vertical);
+
+        body.MovePosition(body.position + move * speed * Time.fixedDeltaTime);
+
+        if (!Mathf.Approximately(move.x, 0f) || !Mathf.Approximately(move.y, 0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+    }
+
+}
+
+
