59ef616c1ec642c721eef54f6f3be611a98990b5
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2019-11-23 09:56:29 +0300

    add sample scene

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    public float distance = 3.0f;
+
+    public float height = 3.0f;
+
+    public float damping = 5.0f;
+
+    public bool followBehind = true;
+
+
+
+    void Update()
+
+    {
+
+        Vector3 wantedPosition;
+
+        if (followBehind)
+
+            wantedPosition = target.TransformPoint(0, height, -distance);
+
+        else
+
+            wantedPosition = target.TransformPoint(0, height, distance);
+
+
+
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+
+    }
+
+}
