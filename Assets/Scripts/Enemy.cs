f03d252f46264daa02158a1996ddd6a1dda804b0
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2019-11-23 09:55:09 +0300

    add zombie

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
