9134edbfa29dc30dbd03dd1af0921392a69737cf
orhunakture <24290999+orhunakture@users.noreply.github.com>
2019-11-26 13:17:42 +0300

    add ammo constraint to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,55 +12,66 @@ public class Player : MonoBehaviour
     Rigidbody2D body;
     Animator animator;
     Text healthText;
+    Text ammoText;
 
     private Vector2 lookDirection = new Vector2(1, 0);
 
     private int health = 5;
+    private int ammo = 15;
     private UnityEvent healthChanged;
+    private UnityEvent ammoChanged;
 
-    public int Health
-    {
+    public int Health {
         get { return health; }
-        set
-        {
+        set {
             health = value;
             healthChanged.Invoke();
         }
     }
 
-#region Unity API
-    void Awake()
-    {
+    public int Ammo {
+        get { return ammo; }
+        set {
+            ammo = value;
+            ammoChanged.Invoke();
+        }
+    }
+
+    #region Unity API
+    void Awake() {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         healthText = GameObject.Find("HealthText").GetComponent<Text>();
+        ammoText = GameObject.Find("AmmoText").GetComponent<Text>();
     }
 
-    void Start()
-    {
+    void Start() {
         if (healthChanged == null)
             healthChanged = new UnityEvent();
+        if (ammoChanged == null)
+            ammoChanged = new UnityEvent();
 
         healthChanged.AddListener(OnHealthChanged);
         healthChanged.AddListener(CheckGameOver);
         OnHealthChanged();
+
+        ammoChanged.AddListener(OnAmmoChanged);
+        OnAmmoChanged();
     }
 
-    void OnDisable()
-    {
+    void OnDisable() {
         healthChanged.RemoveAllListeners();
+        ammoChanged.RemoveAllListeners();
     }
 
-    void Update()
-    {
+    void Update() {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         var move = new Vector2(horizontal, vertical);
 
         body.MovePosition(body.position + move * speed * Time.fixedDeltaTime);
 
-        if (!Mathf.Approximately(move.x, 0f) || !Mathf.Approximately(move.y, 0f))
-        {
+        if (!Mathf.Approximately(move.x, 0f) || !Mathf.Approximately(move.y, 0f)) {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
@@ -71,27 +82,34 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             Launch();
     }
-#endregion
+    #endregion
 
-    void Launch()
-    {
+    void Launch() {
+        if (Ammo == 0)
+            return;
+
         var projectileObject = Instantiate(projectilePrefab, body.position + lookDirection * .5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 600f);
 
         animator.SetTrigger("Shoot");
+
+        Ammo--;
     }
 
-    void OnHealthChanged()
-    {
+    void OnHealthChanged() {
         healthText.text = $"Health: { Health }";
     }
 
-    void CheckGameOver()
-    {
+    void CheckGameOver() {
         if (Health <= 0)
             return;
+    }
+
+    void OnAmmoChanged()
+    {
+        ammoText.text = $"Ammo: { Ammo }";
     }
 }
 
