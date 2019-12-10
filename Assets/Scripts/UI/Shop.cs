d97b6dcf44c0ea8110ce3a989cfddfc7c1dac8b4
orhunakture <24290999+orhunakture@users.noreply.github.com>
2019-12-10 15:20:01 +0300

    add default store scene

---
+++ Assets/Scripts/UI/Shop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [Header("List of items sold")]
+    [SerializeField] private ShopItem[] shopItems;
+
+    [Header("References")]
+    [SerializeField] private Transform shopContainer;
+    [SerializeField] private GameObject shopItemPrefab;
+
+    private void Start()
+    {
+        PopulateShop();
+    }
+
+    private void PopulateShop()
+    {
+        foreach (var item in shopItems)
+        {
+            GameObject itemObject = Instantiate(shopItemPrefab, shopContainer);
+
+            itemObject.transform.Find("Image").GetComponent<Image>().sprite = item.sprite;
+            itemObject.transform.Find("Price").GetComponent<Text>().text = $"${ item.cost }";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ShopItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Shop/ShopItem")]
+public class ShopItem : ScriptableObject
+{
+    public string itemName;
+    public Sprite sprite;
+    public int cost;
+}
