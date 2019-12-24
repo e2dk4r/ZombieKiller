ba48d53b32faa6fa5064a7a56d769e19aab07283
orhunakture <24290999+orhunakture@users.noreply.github.com>
2019-12-24 16:45:33 +0300

    initialize keybindings when game starts

---
+++ Assets/Scripts/KeybindingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybindingController : MonoBehaviour
+{
+    void Start()
+    {
+        Keybindings.InitializePlayerAction(PlayerAction.Up, KeyCode.W);
+        Keybindings.InitializePlayerAction(PlayerAction.Left, KeyCode.A);
+        Keybindings.InitializePlayerAction(PlayerAction.Right, KeyCode.D);
+        Keybindings.InitializePlayerAction(PlayerAction.Down, KeyCode.S);
+        Keybindings.InitializePlayerAction(PlayerAction.Fire, KeyCode.Space);
+    }
+}
